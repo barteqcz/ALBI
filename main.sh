1baa3f815bda09510ac60aa71b232d2548e03850
barteqcz <118764028+barteqcz@users.noreply.github.com>
2023-05-28 18:08:24 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -44,7 +44,7 @@ echo "MS-7817" > /etc/hostname
 # /etc/hosts configuration
 echo "# The following lines are desirable for IPv4 capable hosts" > /etc/hosts
 echo "127.0.0.1       localhost" >> /etc/hosts
-echo "\n" >> /etc/hosts
+echo "" >> /etc/hosts
 echo "# The following lines are desirable for IPv6 capable hosts" >> /etc/hosts
 echo "::1             localhost ip6-localhost ip6-loopback" >> /etc/hosts
 echo "ff02::1         ip6-allnodes" >> /etc/hosts
