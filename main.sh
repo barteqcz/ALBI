9b3ba489bda299351305a7d023733c8e6aec0bd9
barteqcz <118764028+barteqcz@users.noreply.github.com>
2023-05-28 15:29:39 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -42,6 +42,15 @@ echo "KEYMAP=cz" > /etc/vconsole.conf
 # /etc/hostname configuration
 echo "MS-7817" > /etc/hostname
 
+# /etc/hosts configuration
+echo "# The following lines are desirable for IPv4 capable hosts" > /etc/hosts
+echo "127.0.0.1       localhost" >> /etc/hosts
+echo "\n" >> /etc/hosts
+echo "# The following lines are desirable for IPv6 capable hosts" >> /etc/hosts
+echo "::1             localhost ip6-localhost ip6-loopback" >> /etc/hosts
+echo "ff02::1         ip6-allnodes" >> /etc/hosts
+echo "ff02::2         ip6-allrouters" >> /etc/hosts
+
 # User configuration
 useradd -m bartosz
 echo "bartosz:$passwd" | chpasswd
