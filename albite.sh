e27a1047daad28d131592ffa53ec6c08524f99c1
barteqcz <118764028+barteqcz@users.noreply.github.com>
2023-05-29 19:32:32 +0200

    Update albite.sh

--- albite.sh
+++ albite.sh
@@ -9,5 +9,8 @@ genfstab -U /mnt >> /mnt/etc/fstab
 # Copy second part of the script to /
 cp main.sh /mnt/
 
+# Copy config file to /
+cp config.conf /mnt/
+
 # Enter arch-chroot and run second part of the script
 arch-chroot /mnt bash main.sh
