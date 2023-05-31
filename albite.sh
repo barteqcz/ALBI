fbb514ccc27943f85402356bbf35d2b1f80417bd
barteqcz <118764028+barteqcz@users.noreply.github.com>
2023-05-31 11:23:42 +0200

    Update albite.sh

--- albite.sh
+++ albite.sh
@@ -7,6 +7,58 @@ else
     boot_mode="BIOS"
 fi
 
+# Create configuration file based on the boot mode
+if [ -e "config.conf" ]; then
+    echo "Config file already exists"
+else
+    touch config.conf
+    cat <<EOF > config.conf
+# Here is the configuration for the installation. For any needed help, refer to the documentation.
+
+# Timezone setting
+timezone="Europe/Prague"
+
+# User configuration
+username="exampleusername"
+password="examplepasswd"
+
+# Locales settings
+language="cs_CZ.UTF-8"
+console_keyboard_layout="cz"
+
+# Hostname
+hostname="examplehostname"
+
+# GRUB settings
+EOF
+
+if [[ $boot_mode == "UEFI" ]]; then
+    echo 'efi_partition="/boot/efi"' >> config.conf
+else
+    echo 'grub_installation_disk="/dev/sda"' >> config.conf
+fi
+
+cat <<EOF >> config.conf
+# Audio server setting
+audio_server="pipewire"
+
+# GPU driver
+gpu_driver="nvidia"
+
+# DE settings
+de="xfce"
+
+# Decide whether CUPS should be installed
+cups_installation="yes"
+
+# Swapfile settings
+create_swapfile="yes"
+swapfile_size_gb="4"
+EOF
+
+echo "Config file was generated successfully. Edit it to adjust it to your needs."
+fi
+
 # Install base system
 echo "Installing base system..."
 pacstrap -K /mnt base linux linux-firmware linux-headers >/dev/null 2>&1
