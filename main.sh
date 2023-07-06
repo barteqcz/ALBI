499f001504b316a7ff38a8818072d607522c35c1
barteqcz <118764028+barteqcz@users.noreply.github.com>
2023-07-06 12:45:47 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -81,7 +81,7 @@ sed -i 's/^# include "\/usr\/share\/nano\/\*\.nanorc"/include "\/usr\/share\/nan
 sed -i '/Color/s/^#//g' /etc/pacman.conf
 cln=$(grep -n "Color" /etc/pacman.conf | cut -d ':' -f1)
 sed -i "${cln}s/$/\nILoveCandy/" /etc/pacman.conf
-dsln=$(grep -n "## Defaults specification" /etc/sudoers | cut -d ':' -f1)
+dln=$(grep -n "## Defaults specification" /etc/sudoers | cut -d ':' -f1)
 sed -i "${dsln}s/$/\nDefaults    pwfeedback/" /etc/sudoers
 sed -i "${dsln}s/$/\n##/" /etc/sudoers
 
