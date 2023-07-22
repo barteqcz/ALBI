1e2fb969f2b2ea2321d5063b4ed451d540f45ed2
memchr <memchr@proton.me>
2023-07-22 14:35:28 +0000

    feat: support installing aur packages via paru

--- main.sh
+++ main.sh
@@ -73,6 +73,14 @@ echo "$username:$password" | chpasswd
 usermod -aG wheel $username
 sed -i '/%wheel ALL=(ALL:ALL) ALL/s/^#//g' /etc/sudoers
 
+# Install paru
+git clone --depth 1 https://aur.archlinux.org/paru-bin.git
+cd paru-bin
+makepkg -si --noconfirm
+cd ..
+rm -rf paru-bin
+paru -S hplip-plugin --noconfirm
+
 # Apply useful tweaks
 sed -i 's/^# include "\/usr\/share\/nano\/\*\.nanorc"/include "\/usr\/share\/nano\/\*\.nanorc"/' /etc/nanorc
 sed -i '/Color/s/^#//g' /etc/pacman.conf
