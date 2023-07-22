68b318e5e3b69054b3bd68073c28cbf1995e5dcd
barteqcz <118764028+barteqcz@users.noreply.github.com>
2023-07-22 14:55:59 +0200

    Update albite.sh

--- albite.sh
+++ albite.sh
@@ -34,6 +34,9 @@ else
 # Kernel variant
 kernel_variant="normal"
 
+# Mirror servers country (enter english name of the country, or leave auto to let Reflector pick best servers)
+reflector_country="auto"
+
 # Timezone setting
 timezone="Europe/Prague"
 
