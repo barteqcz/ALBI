8445cfad7cf0bf221e47d8b4f2e1400df967c0ad
barteqcz <118764028+barteqcz@users.noreply.github.com>
2023-07-28 00:25:14 +0200

    Update albi.sh

--- albi.sh
+++ albi.sh
@@ -76,7 +76,7 @@ create_swapfile="yes"
 swapfile_size_gb="4"
 
 # Custom packages (separated by spaces)
-custom_packages="firefox git htop neofetch papirus-icon-theme"
+custom_packages="firefox htop neofetch papirus-icon-theme"
 EOF
 
 echo "config.conf was generated successfully. Edit it to customize the installation."
@@ -182,12 +182,13 @@ trap interrupt_handler SIGINT
 source /config.conf
 
 # Set timezone
+echo "Setting the timezone..."
 ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
 hwclock --systohc
 
 # Install basic packages
 echo "Installing basic packages..."
-pacman -Sy base-devel bash-completion bluez bluez-utils nano grub ntfs-3g sshfs networkmanager wget exfat-utils usbutils xdg-utils xdg-user-dirs unzip unrar os-prober --noconfirm >/dev/null 2>&1
+pacman -Sy base-devel bash-completion bluez bluez-utils nano git grub ntfs-3g sshfs networkmanager wget exfat-utils usbutils xdg-utils xdg-user-dirs unzip unrar os-prober --noconfirm >/dev/null 2>&1
 systemctl enable NetworkManager >/dev/null 2>&1
 systemctl enable bluetooth >/dev/null 2>&1
 
