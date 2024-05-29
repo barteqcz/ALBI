5f58a11a87f957c31424f50eb5533f779a5a381c
Bartek <118764028+barteqcz@users.noreply.github.com>
2024-05-29 22:01:28 +0200

    Update albi.sh

--- albi.sh
+++ albi.sh
@@ -94,7 +94,7 @@ custom_packages="firefox htop papirus-icon-theme"  #### Custom packages (space-s
 create_swapfile="yes"  #### Create swapfile (yes/no)
 EOF
 
-total_ram_gb=$(free | awk '/^Mem:/ {printf "%d\n", $2 / 1024 / 1024}')
+total_ram_gb=$(free | awk '/^Mem:/ {printf "%d\n", ($2 / 1024 / 1024) + 0.5}')
 echo "swapfile_size_gb=\"$total_ram_gb\"  #### Swapfile size in GB" >> config.conf
 
 cat <<EOF >> config.conf
