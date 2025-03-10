e87faac5c5819462611f7bf4b9c4c6c2a222a5a5
Bartek <118764028+barteqcz@users.noreply.github.com>
2025-03-10 18:49:08 +0100

    Critical security update

--- albi.sh
+++ albi.sh
@@ -793,7 +793,7 @@ if [[ "$create_swapfile" == "yes" ]]; then
 fi
 
 if [[ "$tpm2_luks" == "yes" ]]; then
-    PASSWORD="$luks_passphrase" systemd-cryptenroll --wipe-slot=tpm2 --tpm2-device=auto "$root_part"
+    PASSWORD="$luks_passphrase" systemd-cryptenroll --wipe-slot=tpm2 --tpm2-device=auto --tpm2-pcrs=0+7+8+9 "$root_part"
 fi
 
 mkinitcpio -P
