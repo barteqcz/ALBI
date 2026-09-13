581d684c9b6441e6d2e38133dd3007bda0cd5bba
Bartek <118764028+barteqcz@users.noreply.github.com>
2026-09-13 12:59:04 +0200

    Update albi.sh

--- albi.sh
+++ albi.sh
@@ -662,8 +662,8 @@ locale-gen
 
 pacman -Sy btrfs-progs dosfstools dnsmasq inetutils xfsprogs base-devel polkit bash-completion nano grub ntfs-3g sshfs exfatprogs usbutils xdg-utils xdg-user-dirs unzip unrar zip 7zip os-prober plymouth --noconfirm
 
-if [[ "$network_management" == "network-manager' ]]; then
-    pacman -S network-manager --noconfirm
+if [[ "$network_management" == "network-manager" ]]; then
+    pacman -S networkmanager --noconfirm
     systemctl enable NetworkManager
 elif [[ "$network_management" == "systemd-networkd" ]]; then
     default_route=$(ip route | grep '^default')
